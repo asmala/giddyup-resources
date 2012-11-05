fb680f76399ae0decbe7b1efaa3ce8d950ff2669
Janne Asmala <janne@asmala.name>
2012-11-05 23:35:24 +0800

    Added minified files to middleware test

--- test/giddyup_resources/middleware_test.clj
+++ test/giddyup_resources/middleware_test.clj
@@ -6,11 +6,15 @@
 (deftest wrap-giddyup-resources-test
   (let [handler (wrap-giddyup-resources (fn [req] {}))
         resources [["/css/bootstrap.css" "text/css"]
+                   ["/css/bootstrap.min.css" "text/css"]
                    ["/css/bootstrap-responsive.css" "text/css"]
+                   ["/css/bootstrap-responsive.min.css" "text/css"]
                    ["/img/glyphicons-halflings.png" "image/png"]
                    ["/img/glyphicons-halflings-white.png" "image/png"]
                    ["/js/bootstrap.js" "text/javascript"]
-                   ["/js/jquery.js" "text/javascript"]]]
+                   ["/js/bootstrap.min.js" "text/javascript"]
+                   ["/js/jquery.js" "text/javascript"]
+                   ["/js/jquery.min.js" "text/javascript"]]]
     (doseq [[resource mime-type] resources]
       (let [response (handler (mock/request :get (str "/giddyup" resource)))]
         (is (= (get-in response [:headers "Content-Type"])
